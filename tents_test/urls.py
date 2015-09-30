aac8e31d37f1a792114e10273edcaa79f1be9699
Nicole M <nicmatts@gmail.com>
2015-09-30 16:39:46 -0500

    Add basic view, url, and template

--- tents_test/urls.py
+++ tents_test/urls.py
@@ -3,8 +3,8 @@ from django.contrib import admin
 
 urlpatterns = [
     # Examples:
-    # url(r'^$', 'tents_test.views.home', name='home'),
+    url(r'^$', 'store.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-
+    url(r'^store/', include('store.urls'), name='store'),
     url(r'^admin/', include(admin.site.urls)),
 ]
