a2d1948ec4d9bee0b0dedc00773f22d3070101c8
Nicole M <nicmatts@gmail.com>
2015-10-01 09:42:34 -0500

    Add test for home page

--- store/tests.py
+++ store/tests.py
@@ -1,3 +1,19 @@
+from django.core.urlresolvers import resolve
 from django.test import TestCase
+from django.http import HttpRequest
+from django.template.loader import render_to_string
 
-# Create your tests here.
+from store.views import home
+
+
+class HomePageTest(TestCase):
+
+    def test_root_url_resolves_to_home_page_view(self):
+        found = resolve('/')
+        self.assertEqual(found.func, home)
+
+    def test_home_page_returns_correct_html(self):
+        request = HttpRequest()
+        response = home(request)
+        expected_html = render_to_string('home.html')
+        self.assertEqual(response.content.decode(), expected_html)
---
+++ store/urls.py
@@ -0,0 +1,9 @@
+from django.conf.urls import include, url
+
+urlpatterns = [
+    # Examples:
+    #url(r'^$', 'store.views.home', name='home'),
+    # url(r'^blog/', include('blog.urls')),
+    #url(r'^store/', include('store.urls'), name='store'),
+    #url(r'^admin/', include(admin.site.urls)),
+]
