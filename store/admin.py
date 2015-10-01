51f02ff91f45bdcee3540532127dd036b8e6d71d
Nicole M <nicmatts@gmail.com>
2015-10-01 10:10:05 -0500

    Add categories

--- store/admin.py
+++ store/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Category
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    verbose_name = "Categories"
+
+admin.site.register(Category, CategoryAdmin)
--- store/models.py
+++ store/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=200)
+    description = models.TextField()
+    category_image = models.ImageField()
+
+    def __unicode__(self):
+        return "%s" % (self.name)
+
+    class Meta:
+        verbose_name = "Category"
+
+
+# class Product(models.Model):
+#     name = models.CharField(max_length=200)
+#     description = models.TextField()
+#     quantity = models.IntegerField(default=1)
+#     product_image = models.ImageField()
+#     price = models.DecimalField(decimal_places=2, max_digits=8)
+
+#     def __unicode__(self):
+#         return "%s" % (self.name)
