ecfe43775fe323e8793d94914f4eee9bf5e9beed
Nicole M <nicmatts@gmail.com>
2015-10-02 09:44:03 -0500

    Test categories page

--- store/tests.py
+++ store/tests.py
@@ -3,7 +3,9 @@ from django.test import TestCase
 from django.http import HttpRequest
 from django.template.loader import render_to_string
 
-from store.views import home
+from .models import Category
+
+from .views import home, categories, category
 
 
 class HomePageTest(TestCase):
@@ -17,3 +19,24 @@ class HomePageTest(TestCase):
         response = home(request)
         expected_html = render_to_string('home.html')
         self.assertEqual(response.content.decode(), expected_html)
+
+    def test_category_page_returns_correct_html(self):
+        request = HttpRequest()
+        response = home(request)
+        expected_html = render_to_string('home.html')
+        self.assertEqual(response.content.decode(), expected_html)
+
+
+class CategoriesPageTest(TestCase):
+    def setUp(self):
+        category = Category.objects.create(name="test", description="test", category_image="test.jpg")
+        category2 = Category.objects.create(name="test2", description="test", category_image="test.jpg")
+
+    def test_categories(self):
+        resp = self.client.get('/products/categories/')
+        # make sure page loads
+        self.assertEqual(resp.status_code, 200)
+        # in the context variable for view, test for 'books' entry
+        self.assertTrue('categories' in resp.context)
+        # check to make sure there is at least one book in the DB
+        self.assertTrue(resp.context['categories'].count > 0)
