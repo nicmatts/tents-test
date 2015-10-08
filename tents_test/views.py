7a4a30f341971750951c04a3fab2f00d3555f614
Nicole M <nicmatts@gmail.com>
2015-10-08 09:10:58 -0500

    Add login and registration links to product page

--- tents_test/views.py
+++ tents_test/views.py
@@ -1,1 +1,8 @@
+# from registration.backends.simple.views import LoginView
 
+
+# class MyLoginView(LoginView):
+
+#     def get_success_url(self, request, user):
+#         current_page = self.request.GET
+#         return current_page
